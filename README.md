81ad14d0503fc21b7ba9dfdb6538c520a1ce1cc5
Wesley Alcoforado <wesleyjefferson@gmail.com>
2011-09-18 19:21:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Kurso
+Kurso is a free esperanto course for Android phones and tablets.
+
+## Copyright
+
+Kurso is Copyright (c) 2011 Wesley Alcoforado and is licensed under the [GPL License](http://www.gnu.org/licenses/gpl.html). 
---
+++ src/org/brazilo/esperanto/kurso/KioEstasActivity.java
@@ -0,0 +1,13 @@
+package org.brazilo.esperanto.kurso;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KioEstasActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.kio_estas);
+	}
+}
