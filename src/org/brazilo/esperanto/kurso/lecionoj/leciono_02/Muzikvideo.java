97434c1e42928d6603a50a397edb4bc414886446
Wesley Alcoforado <wesleyjefferson@gmail.com>
2012-01-22 22:37:49 -0200

    teksto pri la video

--- src/org/brazilo/esperanto/kurso/lecionoj/leciono_02/Muzikvideo.java
+++ src/org/brazilo/esperanto/kurso/lecionoj/leciono_02/Muzikvideo.java
@@ -3,21 +3,19 @@ package org.brazilo.esperanto.kurso.lecionoj.leciono_02;
 import org.brazilo.esperanto.kurso.R;
 
 import android.app.Activity;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.MediaController;
-import android.widget.VideoView;
+import android.view.View;
 
 public class Muzikvideo extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.leciono_2_muzikvideo);
-		
-		VideoView v = (VideoView)findViewById(R.id.video);
-		v.setMediaController(new MediaController(this));
-		v.setVideoURI(Uri.parse("android.resource://"+ getPackageName() +"/" + R.raw.jen));
-		v.requestFocus();
-		v.start();
+		setContentView(R.layout.leciono_2_muzikvideo_teksto);
+	}
+	
+	public void LudiVideon(View v){
+		Intent intent = new Intent(this, Ludilo.class);
+		startActivity(intent);
 	}
 }
